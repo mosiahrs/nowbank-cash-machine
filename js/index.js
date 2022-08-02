c86405993440fb74de7cad74d3df5362376d4dec
Holiv <mrheltonso@gmail.com>
2022-08-01 22:04:06 -0300

    atualização do script

--- js/index.js
+++ js/index.js
@@ -1,57 +1,61 @@
 let bankName = 'Now Bank';
 let saldo = 500;
-let opcaoSelecionada = "saldo";
+let opcaoSelecionada;
 let encerrarPrograma = false;
 
 function mostrarSaldo() {
-    let botaoSaldo = document.querySelector('#saldo').value
-    alert(`${botaoSaldo}`)
+    opcaoSelecionada = 'saldo';
+    switchFunction(opcaoSelecionada);
 };
 
 
-function efetuarSaque(){
-    let saque = document.querySelector('#saque').value
-    alert(`${saque}`)
+function efetuarSaque() {
+    opcaoSelecionada = 'saque';
+    switchFunction(opcaoSelecionada);
 };
 
-function efetuarDeposito(){
-    let deposito = document.querySelector('#deposito').value
-    alert(`${deposito}`)
+function efetuarDeposito() {
+    opcaoSelecionada = 'deposito';
+    switchFunction(opcaoSelecionada);
 };
 
 
-function finalizar(){
-let encerrar = document.querySelector('#encerrar').value
-    alert(`${encerrar}`)
+function finalizar() {
+    opcaoSelecionada = 'encerrar';
+    switchFunction(opcaoSelecionada);
 };
 
-const selecao = opcaoSelecionada
-switch (selecao) {
-    case 'saldo':
-        console.log(`Seu saldo é R$ ${saldo}`);
-        break;
-    case 'saque':
-        let valorSaque = window.Number(prompt(` Digite o valor que deseja sacar "$"  `))
-        if (valorSaque > saldo) {
-            console.log('Saque não autorizado, valor indisponível')
-        } else {
-            saldo = saldo - valorSaque;
-            console.log(` O valor R$ ${valorSaque} foi autorizado`)
-            console.log(`Seu saldo atual é R$ ${saldo}`)
-        }
-        break;
-    case 'deposito':
-        valorDeposito = window.Number(prompt(` Digite o valor que deseja depositar "$" `))
-        saldo = saldo + valorDeposito
-        console.log(`Seu saldo atual é R$ ${saldo}`)
-        break;
-    case 'encerrar':
-        encerrarPrograma = true;
-        console.log(`O ${bankName} agradece a preferência, volte sempre!`)
-        break;
-    default:
-        alert('Entrada inválida, escolha novamente a opção desejada')
+
+const switchFunction = opcao => {
+    const selecao = opcao;
+    switch (selecao) {
+        case 'saldo':
+            alert(`Seu saldo é R$ ${saldo}`);
+            break;
+        case 'saque':
+            let valorSaque = window.Number(prompt(` Digite o valor que deseja sacar "$"  `));
+            if (valorSaque > saldo) {
+                console.log('Saque não autorizado, valor indisponível');
+            } else {
+                saldo = saldo - valorSaque;
+                alert(` O valor R$ ${valorSaque} foi autorizado`);
+                alert(`Seu saldo atual é R$ ${saldo}`);
+            }
+            break;
+        case 'deposito':
+            valorDeposito = window.Number(prompt(` Digite o valor que deseja depositar "$" `));
+            saldo = saldo + valorDeposito
+            alert(`Seu saldo atual é R$ ${saldo}`);
+            break;
+        case 'encerrar':
+            encerrarPrograma = true;
+            alert(`O ${bankName} agradece a preferência, volte sempre!`);
+            break;
+        default:
+            alert('Entrada inválida, escolha novamente a opção desejada');
+    }
 }
+
 
 
 
