88aa45bf4f78529058cdb7e38c6c9b9be6e47246
Mosiah Silva <100864562+mosiahrs@users.noreply.github.com>
2022-08-02 15:20:05 -0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -3,11 +3,37 @@ let saldo = 500;
 let opcaoSelecionada;
 let encerrarPrograma = false;
 
+let depositoContainer = document.getElementById('depositoContainer')
+let saqueContainer = document.getElementById('saqueContainer')
+let intro = document.getElementById('intro')
+
+
+function mostrarElementosDeposito(){
+    intro.style.display ="none";
+    saqueContainer.style.display ="none"
+    depositoContainer.style.display ="block"
+}
+function mostrarElementosSaque(){
+    intro.style.display ="none";
+    depositoContainer.style.display ="none"
+    saqueContainer.style.display ="block"
+}
+
+function calcular(){
+    let entrada = document.getElementById('depositoInput');
+    let saida = document.getElementById('retornoInfo')
+    valor = Number(entrada.value)
+    saldo = saldo + valor;
+    console.log(saldo)
+
+    saida.innerHTML = `O depósito de R$ ${valor} foi efetuado com sucesso`
+}  
+
+//Parametros - p/ Loop de Funcionamento
 function mostrarSaldo() {
     opcaoSelecionada = 'saldo';
     switchFunction(opcaoSelecionada);
 };
-
 
 function efetuarSaque() {
     opcaoSelecionada = 'saque';
@@ -19,20 +45,26 @@ function efetuarDeposito() {
     switchFunction(opcaoSelecionada);
 };
 
-
 function finalizar() {
     opcaoSelecionada = 'encerrar';
     switchFunction(opcaoSelecionada);
 };
 
+//Loop de Funcionamento
 
 const switchFunction = opcao => {
     const selecao = opcao;
     switch (selecao) {
         case 'saldo':
-            alert(`Seu saldo é R$ ${saldo}`);
+            let retorno = document.getElementById('retornoInfo').innerHTML = `
+            <p> 
+            <strong>Você escolheu a opção SALDO</strong> <br>Seu saldo é R$ ${saldo}
+            </p>
+            `
+            console.log(`Seu saldo é R$ ${saldo}`);
             break;
         case 'saque':
+            mostrarElementosSaque()
             let valorSaque = window.Number(prompt(` Digite o valor que deseja sacar "$"  `));
             if (valorSaque > saldo) {
                 console.log('Saque não autorizado, valor indisponível');
@@ -42,20 +74,29 @@ const switchFunction = opcao => {
                 alert(`Seu saldo atual é R$ ${saldo}`);
             }
             break;
-        case 'deposito':
-            valorDeposito = window.Number(prompt(` Digite o valor que deseja depositar "$" `));
-            saldo = saldo + valorDeposito
-            alert(`Seu saldo atual é R$ ${saldo}`);
-            break;
+            case 'deposito':
+                mostrarElementosDeposito()
+                /*valorDeposito = window.Number(prompt(` Digite o valor que deseja depositar "$" `));
+                saldo = saldo + valorDeposito
+                alert(`Seu saldo atual é R$ ${saldo}`);*/
+                break;
         case 'encerrar':
+            let finalizaExecucao = document.getElementById('retornoInfo').innerHTML = `
+            <p> 
+            O <strong> ${bankName} </strong> agradece a preferência, volte sempre!
+            </p>
+            `
             encerrarPrograma = true;
-            alert(`O ${bankName} agradece a preferência, volte sempre!`);
+            console.log(`O ${bankName} agradece a preferência, volte sempre!`);
+            const reset = setTimeout(resetarTela, 2000);
+            function resetarTela(){
+                location.reload();
+            }
             break;
         default:
             alert('Entrada inválida, escolha novamente a opção desejada');
     }
 }
-
 
 
 
